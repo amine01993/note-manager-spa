a2743fe18df1cb597fb3447470455e3d4fcfd4bc
Amine Errafii <err.amine93@gmail.com>
2025-03-02 16:21:08 -0500

    add amplify

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # React + TypeScript + Vite
 
+Welcome to AWS Amplify!
+ - Get started by running npx ampx sandbox.
+ - Run npx ampx help for a list of available commands.
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
 import App from './App.tsx';
+
+Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>

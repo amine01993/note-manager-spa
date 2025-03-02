9c6caf31124bc3759d762cf0e28f5532e64b3234
Amine Errafii <err.amine93@gmail.com>
2025-03-02 16:25:58 -0500

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+// import outputs from "../amplify_outputs.json";
 import App from './App.tsx';
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
